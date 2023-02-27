5b9d85ff93bfb24399bc1684bc439a28ad7ada96
Ben <66697315+Boxit379@users.noreply.github.com>
2023-02-27 11:25:51 -0700

    Added count

---
+++ square.js
@@ -0,0 +1,27 @@
+const square = document.getElementById('square');
+let rotations = 0;
+let squareHovered = false;
+
+// Load from local storage
+if (localStorage.getItem('rotations')) {
+    rotations = parseInt(localStorage.getItem('rotations'));
+    document.getElementById('status').innerHTML = `square. [${rotations}]`;
+}
+
+square.addEventListener('animationiteration', () => {
+    if (!squareHovered) {
+        rotations++;
+        document.getElementById('status').innerHTML = `square. [${rotations}]`;
+        localStorage.setItem('rotations', rotations);
+    }
+});
+
+// On mouse in
+square.addEventListener('mouseover', () => {
+    squareHovered = true;
+});
+
+// On mouse out
+square.addEventListener('mouseout', () => {
+    squareHovered = false;
+});
